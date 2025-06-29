5e66a0eccd13912f491a9bd42bf9eda9bcf0854f
gizon123e <gizon123e@gmail.com>
2025-06-29 12:55:36 +0700

    some adjustment database

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,15 +3,38 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Todo;
+use App\Models\User;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
 
 class UserController extends Controller
 {
     public function profile(Request $request)
     {
+        $todo = Todo::where('user_id', $request->user()->id)->get();
         return response()->json([
             'message' => 'Berhasil mendapatkan data Profile',
-            'data' => $request->user()
+            'data' => [
+                'todo' => $todo,
+                'user' => $request->user()
+            ]
         ]);
+    }
+
+    public function profileById(Request $request, $id)
+    {
+        try{
+            $user = User::findOrFail($id);
+            return response()->json([
+                'message' => 'Berhasil mendapatkan data Profile',
+                'data' => $user
+            ]);
+        }catch(\Exception $e){
+            return response()->json([
+                'message' => 'Gagal mendapatkan data Profile',
+                'data' => $e
+            ]);
+        }
     }
 }
