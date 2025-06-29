4346b361d862fbcd856efc020ca00b1df0826646
gizon123e <gizon123e@gmail.com>
2025-06-29 18:01:14 +0700

    profile orang lain public

--- routes/api/user.php
+++ routes/api/user.php
@@ -2,8 +2,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 
+Route::get('/profile/{id}', [UserController::class, 'profileById']);
 Route::middleware('auth:api')->group(function () {
     Route::get('/profile', [UserController::class, 'profile']);
-    Route::get('/profile/{id}', [UserController::class, 'profileById']);
 
 });
